1ad36ccd53386843bce381ef0a3c9e007779ce1a
Fritz Anderson <fritza@mac.com>
2015-06-07 15:14:23 -0500

    Initial loadout for a non-document OS X app in Swift.

---
+++ SwiftGenerator/SwiftGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftGenerator
+//
+//  Created by Fritz Anderson on 6/7/15.
+//  Copyright (c) 2015 Fritz Anderson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftGenerator/SwiftGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftGenerator
+//
+//  Created by Fritz Anderson on 6/7/15.
+//  Copyright (c) 2015 Fritz Anderson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
